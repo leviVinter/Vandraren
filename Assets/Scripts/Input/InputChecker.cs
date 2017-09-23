074042415fb3c844e902ef073d6c4fc754b2dd41
Thomas Hedlund <thomas.takyon@gmail.com>
2017-09-23 16:21:56 +0200

    Create InputChecker

---
+++ Assets/Scripts/Input/InputChecker.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace Vandraren.Inputs
+{
+    public enum ButtonPressType
+    {
+        Down,
+        Pressed,
+        Up
+    }
+
+    public class InputChecker
+    {
+        private Dictionary<ButtonName, Action> _ButtonsDown;
+        private Dictionary<ButtonName, Action> _ButtonsPressed;
+        private Dictionary<ButtonName, Action> _ButtonsUp;
+
+        public InputChecker()
+        {
+            _ButtonsDown = new Dictionary<ButtonName, Action>();
+            _ButtonsPressed = new Dictionary<ButtonName, Action>();
+            _ButtonsUp = new Dictionary<ButtonName, Action>();
+        }
+
+        public void Check()
+        {
+            CheckButtonsDown();
+            CheckButtonsPressed();
+            CheckButtonsUp();
+        }
+
+        public void CheckButtonsDown()
+        {
+            foreach (KeyValuePair<ButtonName, Action> button in _ButtonsDown)
+            {
+                if (InputHandler.GetButtonDown(button.Key))
+                {
+                    button.Value();
+                }
+            }
+        }
+
+        public void CheckButtonsPressed()
+        {
+            foreach (KeyValuePair<ButtonName, Action> button in _ButtonsPressed)
+            {
+                if (InputHandler.GetButton(button.Key))
+                {
+                    button.Value();
+                }
+            }
+        }
+
+        public void CheckButtonsUp()
+        {
+            foreach (KeyValuePair<ButtonName, Action> button in _ButtonsUp)
+            {
+                if (InputHandler.GetButtonUp(button.Key))
+                {
+                    button.Value();
+                }
+            }
+        }
+
+        public void AddInputCheck(ButtonName pButton, Action pCallback, ButtonPressType pPressType)
+        {
+            switch(pPressType)
+            {
+                case ButtonPressType.Down:
+                    _ButtonsDown[pButton] = pCallback;
+                    break;
+                case ButtonPressType.Pressed:
+                    _ButtonsPressed[pButton] = pCallback;
+                    break;
+                case ButtonPressType.Up:
+                    _ButtonsUp[pButton] = pCallback;
+                    break;
+                default:
+                    new ArgumentException("The button press type is not accounted for");
+                    break;
+            }
+        }
+    }
+}
