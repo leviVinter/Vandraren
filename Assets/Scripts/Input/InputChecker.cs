7615e65db6909837ec9f0cf9da95305e8f1e6d1e
Thomas Hedlund <thomas.takyon@gmail.com>
2017-09-23 19:36:55 +0200

    Small change after merge

--- Assets/Scripts/Input/InputChecker.cs
+++ Assets/Scripts/Input/InputChecker.cs
@@ -38,6 +38,7 @@ namespace Vandraren.Inputs
             CheckButtonsDown();
             CheckButtonsPressed();
             CheckButtonsUp();
+            CheckAxis();
         }
 
         /// <summary>
