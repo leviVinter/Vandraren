04119018fa3eb4fa7e43497bc2dfb104a1eb3149
Thomas Hedlund <thomas.takyon@gmail.com>
2017-09-24 13:09:04 +0200

    Create camera controller

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vandraren.World.Characters;
+
+namespace Vandraren.Camera
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform _Follow;
+
+        private void Start()
+        {
+            
+        }
+    }
+}
