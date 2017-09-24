566366d7741bcce5237f24e09dcdbd496c905589
Canjono <michael.hedlund88@gmail.com>
2017-09-24 15:06:56 +0200

    Position the chat bubble

--- Assets/Scripts/World/UI/ChatBubble.cs
+++ Assets/Scripts/World/UI/ChatBubble.cs
@@ -8,20 +8,57 @@ namespace Vandraren.UI
     public class ChatBubble : MonoBehaviour
     {
         private Text _Text;
+        private Transform _Owner;
+        private RectTransform _RectTransform;
+        private float _OffsetY;
+        private float _OffsetX;
         
         public string Text
         {
             set { _Text.text = value; }
         }
 
+        public Transform Owner
+        {
+            set
+            {
+                _Owner = value;
+                _OffsetY = 100;
+                _OffsetX = 0f;
+                UpdatePosition();
+            }
+        }
+
         private void Awake()
         {
             _Text = GetComponentInChildren<Text>();
+            _RectTransform = GetComponent<RectTransform>();
         }
 
         public void Close()
         {
             Destroy(gameObject);
+        }
+
+        public Vector2 Position
+        {
+            set
+            {
+                _RectTransform.position = value;
+            }
+        }
+
+        private void Update()
+        {
+            UpdatePosition();
+        }
+
+        private void UpdatePosition()
+        {
+            var camera = Camera.main;
+            var owner = _Owner;
+            var rect = _RectTransform;
+            _RectTransform.position = Camera.main.WorldToScreenPoint(_Owner.position) + new Vector3(_OffsetX, _OffsetY, 0);
         }
     }
 }
--- Assets/Scripts/World/UI/MessageManager.cs
+++ Assets/Scripts/World/UI/MessageManager.cs
@@ -7,20 +7,33 @@ namespace Vandraren.UI
     public class MessageManager
     {
         private static ChatBubble _ChatBubblePrefab;
+        private static List<ChatBubble> ChatBubbles = new List<ChatBubble>();
 
         public static void OpenChatBubble(string pText, Transform pSpeaker)
         {
             if (_ChatBubblePrefab == null)
             {
-                _ChatBubblePrefab = Resources.Load("Prefabs/ChatBubble", typeof(ChatBubble)) as ChatBubble);
+                _ChatBubblePrefab = Resources.Load("Prefabs/ChatBubble", typeof(ChatBubble)) as ChatBubble;
             }
 
             GameObject canvas = GameObject.FindGameObjectWithTag("MainCanvas");
 
             ChatBubble bubble = GameObject.Instantiate(_ChatBubblePrefab, canvas.transform) as ChatBubble;
             bubble.Text = pText;
-            Vector2 position = pSpeaker.position;
-            bubble.transform.position = position;
+            bubble.Owner = pSpeaker;
+            bubble.Position = Camera.main.WorldToScreenPoint(pSpeaker.position);
+
+            ChatBubbles.Add(bubble);
+        }
+
+        public static void CloseChatBubble(Transform pSpeaker)
+        {
+            ChatBubble bubble = ChatBubbles.Find(a => a.Owner = pSpeaker);
+
+            if (bubble != null)
+            {
+                bubble.Close();
+            }
         }
     }
 }
